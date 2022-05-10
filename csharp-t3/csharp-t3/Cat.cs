04c8724881f6aa80426a537bcf1155d65d42312b
duyanh14 <duyanhnguyen2001@gmail.com>
2022-05-11 05:43:50 +0700

    Cat class

---
+++ csharp-t3/csharp-t3/Cat.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_t3
+{
+
+    public class Cat
+    {
+        private string name = String.Empty;
+        private DateTime age;
+        private int color;
+
+        // Struct dùng để lưu trữ các dữ liệu, không yêu cầu kế thừa
+        private struct Parent
+        {
+            public string dad = string.Empty;
+            public string mom = string.Empty;
+
+            public Parent()
+            {
+            }
+        }
+
+        private Parent parent = new Parent();
+
+        public Cat(string name, DateTime age, int color) 
+        {
+            this.name = name;
+            this.age = age; 
+            this.color = color; 
+        }
+
+        public void setParent(string dad, string mom)
+        {
+            this.parent.dad = dad;
+            this.parent.mom = mom;
+        }
+
+        public void showInfo()
+        {
+            Console.WriteLine("Name = {0}, Age = {1}, Color = {2}",this.name, this.age,this.color);
+            Console.WriteLine("Dad = {0}, Mom = {1}", this.parent.dad, this.parent.mom);
+        }
+
+    }
+}
--- csharp-t3/csharp-t3/Program.cs
+++ csharp-t3/csharp-t3/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using csharp_t3;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,13 +7,20 @@ using System.Threading.Tasks;
 
 namespace csharpt3
 {
+    enum CatColor
+    {
+        Red = 1,
+        Blue = 2,
+        Green = 3,
+        Pink = 4
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            string message = "csharp-t3";
-
-            Console.WriteLine(message);
+            Cat cat = new Cat("Doja", new DateTime(2022, 2, 10), (int)CatColor.Red);
+            cat.setParent("BinZ","Juicy");
+            cat.showInfo();
         }
     }
 }
